3b26681cb686585edea5825aeec6e59bc4627efc
Lucas Viana <lucasviana.dev@gmail.com>
2024-01-11 20:29:01 -0300

    feat: optmizing load images

--- src/app/components/poster/poster.component.ts
+++ src/app/components/poster/poster.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core'
+import { NgOptimizedImage } from '@angular/common'
+
 import { MovieData } from '../../services/@types/MovieData'
 
 @Component({
   selector: 'app-poster',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './poster.component.html',
   styleUrl: './poster.component.scss',
 })
