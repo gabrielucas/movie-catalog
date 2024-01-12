578ec9ac48f1711d74a56bf5557c14bdd45fd692
Lucas Viana <lucasviana.dev@gmail.com>
2024-01-12 10:18:36 -0300

    feat: configuring route to move details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router'
 
 import { HomeComponent } from './pages/home/home.component'
+import { MovieComponent } from './pages/movie/movie.component'
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Catálogo de filmes',
+  },
+  {
+    path: 'movie/:id',
+    component: MovieComponent,
+    title: 'Detalhes do filme',
   },
 ]
--- src/app/components/poster/poster.component.ts
+++ src/app/components/poster/poster.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core'
 import { NgOptimizedImage } from '@angular/common'
 
 import { MovieData } from '../../services/@types/MovieData'
+import { RouterLink, RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-poster',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink, RouterOutlet],
   templateUrl: './poster.component.html',
   styleUrl: './poster.component.scss',
 })
