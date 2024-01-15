146d15933d25efab21247cc5c9dfcf6f23409702
Lucas Viana <lucasviana.dev@gmail.com>
2024-01-15 12:46:31 -0300

    feat: add icon in flag

--- src/app/components/indicators-flags/indicators-flags.component.ts
+++ src/app/components/indicators-flags/indicators-flags.component.ts
@@ -1,16 +1,22 @@
 import { NgStyle } from '@angular/common'
 import { Component, Input, OnInit } from '@angular/core'
+import {
+  IconDefinition,
+  faChartSimple,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { MovieDetails } from '../../services/tmdb-service/@types'
 
 @Component({
   selector: 'app-indicators-flags',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, FontAwesomeModule],
   templateUrl: './indicators-flags.component.html',
   styleUrl: './indicators-flags.component.scss',
 })
 export class IndicatorsFlagsComponent implements OnInit {
+  chartIcon: IconDefinition = faChartSimple
   colorByRelevance!: string
   @Input() movieFlags!: Pick<
     MovieDetails,
